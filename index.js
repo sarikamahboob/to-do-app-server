8f5e27d3021052fd48a4e6a1621a31c9fcc15b24
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-05-19 00:50:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmlny.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const taskCollection = client.db("to-do-app").collection("tasks");
+
+    app.get("/tasks", async (req, res) => {
+      const query = req.query;
+      const cursor = await taskCollection.find(query).toArray();
+      res.send(cursor);
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
+
+      app.delete("/tasks/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`To Do app listening on port ${port}`);
+});
